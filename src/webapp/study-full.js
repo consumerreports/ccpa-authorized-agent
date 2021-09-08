3afc94490f71a1e0c98e515c6a8a26cc2d7b6044
Ryan Rix <ryan@whatthefuck.computer>
2021-09-08 15:47:09 -0700

    studyFullCheck slightly more legible + robust
    checks for any form of TruE TrUe etc, and is probably more legible
    this way even if it's a bit more repetetetitive

--- src/webapp/study-full.js
+++ src/webapp/study-full.js
@@ -3,13 +3,15 @@
 const {ENABLE_STUDY_FULL} = process.env;
 
 const studyFullCheck = (req, res, openCb) => {
-    if (ENABLE_STUDY_FULL == 'true' ||
-        ENABLE_STUDY_FULL == 'True' ||
-        ENABLE_STUDY_FULL == '1') {
-        res.render('full');
-    } else {
-        openCb();
-    }
+  if (ENABLE_STUDY_FULL == undefined) {
+    return openCb();
+  } else if (ENABLE_STUDY_FULL.toLowerCase() == 'true') {
+    return res.render('full');
+  } else if (ENABLE_STUDY_FULL == '1') {
+    return res.render('full');
+  } else {
+    return openCb();
+  }
 };
 
 module.exports = { studyFullCheck };
